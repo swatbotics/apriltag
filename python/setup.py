f1e1f3315743a08c2eb44b11d71907911c8898dd
ibarbech <ibarbech@alumnos.unex.es>
2018-10-15 13:41:20 +0200

    Created the python module to be able to import from the library from outside the directory. The installation of the module is done with the command: sudo python setup.py install

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="apriltag",
+      version="0.1",
+      description="apriltag Python Library",
+      author="Matt zucker",
+      license="GPL",
+      scripts=["apriltag.py"],
+      packages=["."]
+      )
+
